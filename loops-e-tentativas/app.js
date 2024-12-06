80bb9d81205c0aac98c33cdb00ac4acf053670b3
Victor-Valedev <victorvaledev@gmail.com>
2024-12-06 17:37:13 -0300

    Módulo de loops e tentativas

---
+++ loops-e-tentativas/app.js
@@ -0,0 +1,38 @@
+// 1. Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+// let contador = 1; // Inicializa o contador com 1
+
+// while (contador <= 10) { // O loop vai até 10
+//     console.log(contador); // Exibe o valor do contador
+//     contador++; // Incrementa o contador em 1 a cada iteração
+// }
+
+// 2. Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+// let contador2 = 10; // Inicializa o contador com 10
+
+// while (contador2 >= 0) { // O loop vai até 0
+//     console.log(contador2); // Exibe o valor do contador
+//     contador2--; // Decrementa o contador em 1 a cada iteração
+// }
+
+// 3. Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+// O número será inserido diretamente pelo usuário na janela do prompt
+// let numeroDigitado = prompt('Digite um número para a contagem regressiva:'); // Pede ao usuário o número para iniciar a contagem
+
+// while (numeroDigitado >= 0) { // Continua enquanto o número for maior ou igual a 0
+//     console.log(numeroDigitado); // Exibe o número atual
+//     numeroDigitado--; // Decrementa o número em 1 a cada iteração
+// }
+
+// 4. Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+// let numeroDigitado2 = prompt('Digite um número para a contagem progressiva:'); // Pede ao usuário o número para iniciar a contagem
+
+// let contador3 = 0; // Inicializa o contador em 0
+
+// while (contador3 <= numeroDigitado2) { // Continua enquanto o contador for menor ou igual ao número fornecido
+//     console.log(contador3); // Exibe o valor do contador
+//     contador3++; // Incrementa o contador em 1 a cada iteração
+// }
+
+
+
+
