2a178db7e2530bcb0d948b50383b620cce9e3635
Victor-Valedev <victorvaledev@gmail.com>
2024-12-09 16:26:30 -0300

    Corrigido erros ortográficos, e adicionado ponto e vírgula onde estava faltando

--- logica-js-projeto_inicial/Desafio-js/app.js
+++ logica-js-projeto_inicial/Desafio-js/app.js
@@ -1,5 +1,5 @@
 //Exibe uma alerta no navegador
-alert('Boas vindas ao nosso site');
+alert('Boas vindas ao nosso site!');
 
 //let nome = "Lua";
 //let idade = 25;
@@ -8,21 +8,21 @@ alert('Boas vindas ao nosso site');
 let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
 
-let mensagemErro = "Erro! Preencha todos os campos"
+let mensagemErro = "Erro!, Preencha todos os campos.";
 
 //mostrando valor que contém na variável
 alert(mensagemErro);
 
 //Armazenando nome e idade nas respectivas variáveis
 let nome = prompt('Qual é o seu nome?');
-let idade = prompt('Qual a sua idade?');
+let idade = prompt('Qual é a sua idade?');
 
 //mostrando no console do inpecionar do navegador
-console.log('Olá ' + nome + ' você tem ' + idade + ' anos certo?');
+console.log('Olá, ' + nome + ', Você tem ' + idade + ' anos, certo?');
 
 //Verificando se a idade é maior que 18, e mostrando um log caso a condição seja verdadeira
 if(idade > 18){
     console.log('Pode tirar a habilitação.');
 }else{
-    console.log('Ainda não pode tirar a habilitação');
+    console.log('Ainda não pode tirar a habilitação.');
 }
