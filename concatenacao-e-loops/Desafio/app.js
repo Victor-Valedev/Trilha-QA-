ef8ebd1dfc1484e9f2d418d43c169432387c0678
Victor-Valedev <victorvaledev@gmail.com>
2024-12-09 16:16:41 -0300

    Corrigido erros ortográficos.

--- concatenacao-e-loops/Desafio/app.js
+++ concatenacao-e-loops/Desafio/app.js
@@ -3,32 +3,32 @@
 
 let diaDaSemana = prompt('Qual é o dia da semana?');
 
-if(diaDaSemana == 'Sábado' || diaDaSemana == 'Domingo' ){
-    alert('Bom final de semana');
-}else{
+if(diaDaSemana == 'Sábado' || diaDaSemana == 'Domingo' ) {
+    alert('Bom final de semana!');
+} else {
     alert('Boa semana!');
 }
 
 //Desafio 02
 /* Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando. */
 
-let valordigitado = prompt('Digite um número');
+let valordigitado = prompt('Digite um número: ');
 
-if(valordigitado > 0){
+if(valordigitado > 0) {
     alert('O número digitado é positivo');
-}else{
+} else {
     alert('O número digitado é negativo');
 }
 
 //Desafio 03
 /* Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.". */
 
-let pontuacao = prompt('Digite sua pontuação:');
+let pontuacao = prompt('Digite sua pontuação: ');
 
-if(pontuacao >= 100){
-    alert('Parabéns, você venceu!');
-}else{
-    alert('Tente novamente para ganhar');
+if(pontuacao >= 100) {
+    alert('Parabéns! Você venceu!');
+} else {
+    alert('Tente novamente para ganhar.');
 }
 
 //Desafio 04
@@ -37,7 +37,7 @@ if(pontuacao >= 100){
 let saldoAtual = 880;
 
 //Duas opções diferentes
-alert('Seu saldo atual é ' + saldoAtual);
+alert('Seu saldo atual é: ' + saldoAtual);
 alert(`Seu saldo atual é: ${saldoAtual}`);
 
 //Desafio 05
@@ -45,4 +45,4 @@ alert(`Seu saldo atual é: ${saldoAtual}`);
 
 let nome = prompt('Digite seu nome: ');
 
-alert(`Olá, ${nome}, seja bem-vindo! `);
+alert(`Olá, ${nome}, Seja bem-vindo!`);
