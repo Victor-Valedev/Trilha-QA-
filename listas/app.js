aac8dde1c289b6850cb6f879deb30a2302a13f9f
Victor-Valedev <victorvaledev@gmail.com>
2024-12-10 00:04:01 -0300

    Módulo de listas

---
+++ listas/app.js
@@ -0,0 +1,21 @@
+//01 - 
+let listaGenerica = [];
+
+//02 - 
+let listaLinguagens = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//03 -
+listaGenerica.push('Java','Ruby','GoLang');
+
+
+//04 
+let listaNomes1 = ['Victor', 'Gabriel', 'Luiz'];
+console.log(listaNomes1);
+
+//05
+let listaNomes2 = ['Vale', 'João', 'Maria'];
+console.log(listaNomes2[1]);
+
+//06 - 
+let listaNomes3 = ['Zé', 'Pedro', 'Julia'];
+console.log(listaNomes3[2]);
