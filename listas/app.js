7a71102856d58825b60888919378a31f0e24808e
Victor-Valedev <victorvaledev@gmail.com>
2024-12-10 00:06:42 -0300

    Colocado enunciado das questões dos desafios

--- listas/app.js
+++ listas/app.js
@@ -1,21 +1,21 @@
-//01 - 
+//01 - Crie uma lista vazia, com o nome listaGenerica.
 let listaGenerica = [];
 
-//02 - 
+//02 - Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
 let listaLinguagens = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
 
-//03 -
+//03 - Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
 listaGenerica.push('Java','Ruby','GoLang');
 
 
-//04 
+//04 - Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
 let listaNomes1 = ['Victor', 'Gabriel', 'Luiz'];
 console.log(listaNomes1);
 
-//05
+//05 - Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
 let listaNomes2 = ['Vale', 'João', 'Maria'];
 console.log(listaNomes2[1]);
 
-//06 - 
+//06 - Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
 let listaNomes3 = ['Zé', 'Pedro', 'Julia'];
 console.log(listaNomes3[2]);
